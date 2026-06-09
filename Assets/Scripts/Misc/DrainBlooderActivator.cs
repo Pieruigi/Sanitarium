449455d0652b157aae79dbe022a83e61eb2c0cf9
Pieruigi <pierluigi.zomparelli@gmail.com>
2026-06-09 16:37:15 +0200

    a3 completed

---
+++ Assets/Scripts/Misc/DrainBlooderActivator.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Baloon
+{
+    public class DrainBlooderActivator : MonoBehaviour
+    {
+        [SerializeField]
+        BlooderController blooder;
+
+        [SerializeField]
+        List<GameObject> activateList;
+
+        [SerializeField]
+        List<GameObject> deactivateList;
+
+        private void Awake()
+        {
+            foreach (GameObject go in activateList)
+                go.SetActive(false);
+            
+        }
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnEnable()
+        {
+            BlooderController.OnSealed += HandleOnBlooderSelead;
+        }
+
+        private void OnDisable()
+        {
+            BlooderController.OnSealed -= HandleOnBlooderSelead;
+        }
+
+        private void HandleOnBlooderSelead(BlooderController blooderController)
+        {
+            foreach (GameObject go in activateList)
+                go.SetActive(true);
+            
+            foreach (GameObject go in deactivateList)
+                go.SetActive(false);
+        }
+    }
+}
